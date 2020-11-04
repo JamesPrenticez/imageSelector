841883294fa07d44c8ddb774dd42eaf1a724c40a
JamesPrenticez <Jamesprenticez@gmail.com>
2020-11-05 10:53:24 +1300

    Working just need to fit to page

--- client/components/PageScale.jsx
+++ client/components/PageScale.jsx
@@ -3,42 +3,32 @@ import React from 'react'
 function PageScale() {
 
     const setScale = () => {
-        var scale = document.getElementById("scaleSelect")
-        var value = scaleSelect.options[scaleSelect.selectedIndex].value
-        // document.getElementsByClassName("page").style.transform = `scale(${value})`; 
-        document.getElementById("pagea").style.transform = `scale(${value})`
-        alert({value})
+        var value = scaleSelect.options[scaleSelect.selectedIndex].value;
+        document.getElementById("book").style.transform = `scale(${value})`    
     }
-
 
     return (
         <>
-            <div id="pagea"></div>
-
-            <div class="book">
-                <div class="page">
-                    <div class="subpage">Page 1/2</div>
+<div id="book">
+                <div className="page" id="page">
+                    <div className="subpage">Page 1/2</div>
                 </div>
-                <div class="page">
-                    <div class="subpage">Page 2/2</div>
+                <div className="page" id="page">
+                    <div className="subpage">Page 2/2</div>
                 </div>
-            </div>
+</div>
 
-<div class="nav">
-            <select id="scaleSelect" title="Zoom" tabindex="23" data-l10n-id="zoom" onChange={setScale}>
-                    <option id="pageAutoOption" title="" value="auto" selected="selected" data-l10n-id="page_scale_auto">Automatic Zoom</option>
-                    <option id="pageActualOption" title="" value="page-actual" data-l10n-id="page_scale_actual">Actual Size</option>
-                    <option id="pageFitOption" title="" value="page-fit" data-l10n-id="page_scale_fit">Page Fit</option>
-                    <option id="pageWidthOption" title="" value="page-width" data-l10n-id="page_scale_width">Page Width</option>
-                    <option id="customScaleOption" title="" value="custom" disabled="disabled" hidden="true"></option>
-                    <option title="" value="0.5" data-l10n-id="page_scale_percent" data-l10n-args="{ &quot;scale&quot;: 50 }">50%</option>
-                    <option title="" value="0.75" data-l10n-id="page_scale_percent" data-l10n-args="{ &quot;scale&quot;: 75 }">75%</option>
-                    <option title="" value="1" data-l10n-id="page_scale_percent" data-l10n-args="{ &quot;scale&quot;: 100 }">100%</option>
-                    <option title="" value="1.25" data-l10n-id="page_scale_percent" data-l10n-args="{ &quot;scale&quot;: 125 }">125%</option>
-                    <option title="" value="1.5" data-l10n-id="page_scale_percent" data-l10n-args="{ &quot;scale&quot;: 150 }">150%</option>
-                    <option title="" value="2" data-l10n-id="page_scale_percent" data-l10n-args="{ &quot;scale&quot;: 200 }">200%</option>
-                    <option title="" value="3" data-l10n-id="page_scale_percent" data-l10n-args="{ &quot;scale&quot;: 300 }">300%</option>
-                    <option title="" value="4" data-l10n-id="page_scale_percent" data-l10n-args="{ &quot;scale&quot;: 400 }">400%</option>
+<div className="nav">
+            <select id="scaleSelect" title="Zoom" tabIndex="1" onChange={setScale}>
+                    <option id="pageAutoOption" value="auto">Automatic Zoom</option>
+                    <option id="pageActualOption" value="page-actual">Actual Size</option>
+                    <option id="pageFitOption" value="page-fit">Page Fit</option>
+                    <option id="100" value="1">100%</option>
+                    <option id="125" value="1.25">125%</option>
+                    <option id="150" value="1.5">150%</option>
+                    <option id="200" value="2">200%</option>
+                    <option id="300" value="3">300%</option>
+                    <option id="400" value="4">400%</option>
             </select>
 </div>
 

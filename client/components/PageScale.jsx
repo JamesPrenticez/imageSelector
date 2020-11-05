e93ffaba4f715c8aa39c5de0157dfb6e760b9f0a
JamesPrenticez <Jamesprenticez@gmail.com>
2020-11-05 13:11:15 +1300

    looking good

--- client/components/PageScale.jsx
+++ client/components/PageScale.jsx
@@ -4,12 +4,26 @@ function PageScale() {
 
     const setScale = () => {
         var value = scaleSelect.options[scaleSelect.selectedIndex].value;
-        document.getElementById("book").style.transform = `scale(${value})`    
+        document.getElementById("mainWrapper").style.transform = `scale(${value})`    
     }
 
     return (
         <>
-<div id="book">
+<div className="nav">
+            <select id="scaleSelect" title="Zoom" tabIndex="1" onChange={setScale}>
+                    <option id="pageAutoOption" value="auto">Automatic Zoom</option>
+                    <option id="pageActualOption" value="page-actual">Actual Size</option>
+                    <option id="pageFitOption" value="page-fit">Page Fit</option>
+                    <option id="50" value="50">50%</option>
+                    <option id="75" value="75">75%</option>
+                    <option id="100" value="100">100%</option>
+                    <option id="125" value="125">125%</option>
+                    <option id="150" value="150">150%</option>
+                    <option id="200" value="200">200%</option>
+            </select>
+</div>
+
+<div id="mainWrapper">
                 <div className="page" id="page">
                     <div className="subpage">Page 1/2</div>
                 </div>
@@ -18,19 +32,6 @@ function PageScale() {
                 </div>
 </div>
 
-<div className="nav">
-            <select id="scaleSelect" title="Zoom" tabIndex="1" onChange={setScale}>
-                    <option id="pageAutoOption" value="auto">Automatic Zoom</option>
-                    <option id="pageActualOption" value="page-actual">Actual Size</option>
-                    <option id="pageFitOption" value="page-fit">Page Fit</option>
-                    <option id="100" value="1">100%</option>
-                    <option id="125" value="1.25">125%</option>
-                    <option id="150" value="1.5">150%</option>
-                    <option id="200" value="2">200%</option>
-                    <option id="300" value="3">300%</option>
-                    <option id="400" value="4">400%</option>
-            </select>
-</div>
 
         </>
     )

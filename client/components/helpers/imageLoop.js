426a28ccbf1797f1460a947e03addaa48251ccbc
JamesPrenticez <Jamesprenticez@gmail.com>
2020-11-06 09:42:59 +1300

    WIP image loop

---
+++ client/components/helpers/imageLoop.js
@@ -0,0 +1,20 @@
+export function loopImages(name) {
+    for (let i = 0; i < arr.length; i++){
+        const row = document.querySelector('.row');
+        const newImage = document.createElement("div", [className=name])
+
+    }
+
+    
+    
+    return 
+}
+
+
+<div className={jobName}>
+<div className="content">
+<img src={"./img/houses/" + `${jobName}` + "/" + `${jobName}` + ".png"} alt={jobName}></img>
+<h4>{jobName}</h4>
+<p>test</p>
+</div>
+</div>
